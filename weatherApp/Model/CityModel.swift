303002ee188ed216ffcb4d917115397255f99ea9
danilo costa <costadanilo12@gmail.com>
2021-09-20 13:11:01 +0100

    added missing model

---
+++ weatherApp/Model/CityModel.swift
@@ -0,0 +1,15 @@
+//
+//  City.swift
+//  weatherApp
+//
+//  Created by Danilo Costa on 17/09/2021.
+//
+
+import Foundation
+
+struct CityModel {
+    var name: String?
+    var lat: String?
+    var long: String?
+}
+    
